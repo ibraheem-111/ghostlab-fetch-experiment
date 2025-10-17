99e9c14f52d89455e80d5dfa24ed4c4505156f56
ibraheem-111 <ibraheemsal@outlook.com>
2025-10-17 12:33:59 -0700

    added cmd vel file

---
+++ cmd_vel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+
+def publish_cmd_vel():
+    rospy.init_node('cmd_vel_publisher', anonymous=True)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(3)  # 3 Hz
+
+    msg = Twist()
+    msg.linear.x = 0.2
+    msg.linear.y = 0.0
+    msg.linear.z = 0.0
+    msg.angular.x = 0.0
+    msg.angular.y = 0.0
+    msg.angular.z = 0.0
+
+    rospy.loginfo("Publishing /cmd_vel messages at 3Hz...")
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        publish_cmd_vel()
+    except rospy.ROSInterruptException:
+        pass
+
