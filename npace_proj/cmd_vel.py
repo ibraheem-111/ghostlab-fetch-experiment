6cd640eefddebbc67bbc78c1487108c88dd65250
fetchuser <fetchuser@en4144842l.fulton.ad.asu.edu>
2025-10-24 19:29:06 -0700

    added shell comand in comments

--- npace_proj/cmd_vel.py
+++ npace_proj/cmd_vel.py
@@ -1,7 +1,8 @@
 import os
 
-def send_vel_comand(vel_mag, ang_vel):
+def send_vel_comand(vel_mag=0.0, ang_vel=0.0):
 
+    # rostopic pub /cmd_vel geometry_msgs/Twist -r 3 '[-0.2, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
     # cmd = f"rostopic pub /cmd_vel geometry_msgs/Twists -r 3 - '[{vel_mag:.2f} 0.0 0.0]' '[0.0 0.0 {ang_vel:.2f}]'"
     cmd = "rostopic pub /cmd_vel geometry_msgs/Twist -r 3 '[{}, 0.0, 0.0]' '[0.0, 0.0, {}]'".format(vel_mag, ang_vel)
 
