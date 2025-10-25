4d288b29d1a2421352d8a2924d44b249160a963f
Muhammad Ibraheem <125667543+ibraheem-111@users.noreply.github.com>
2025-10-24 19:43:49 -0700

    Update mocap_data.py

---
+++ ros_env/mocap_data.py
@@ -0,0 +1,49 @@
+import rospy
+import csv
+import tf
+from geometry_msgs.msg import PoseStamped
+
+data_list = []
+
+def callback(msg):
+    pose = msg.pose
+
+    quaternion = (
+        pose.orientation.x,
+        pose.orientation.y,
+        pose.orientation.z,
+        pose.orientation.w
+    )
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+    alpha, beta, gamma = euler
+
+    data = {
+        "x": pose.position.x,
+        "y": pose.position.y,
+        "z": pose.position.z,
+        "alpha": alpha,
+        "beta": beta,
+        "gamma": gamma
+    }
+
+    data_list.append(data)
+    rospy.loginfo("Collected: %s", data)
+
+def save_to_csv():
+    rospy.loginfo("Saving data to CSV...")
+    with open("pose_data.csv", "w") as f:
+        writer = csv.DictWriter(f, fieldnames=["x", "y", "z", "alpha", "beta", "gamma"])
+        writer.writeheader()
+        writer.writerows(data_list)
+    rospy.loginfo("Saved %d entries to pose_data.csv", len(data_list))
+    rospy.loginfo("Exiting gracefully.")
+
+def listener():
+    rospy.init_node('pose_collector', anonymous=True)
+    rospy.Subscriber("/vrpn_client_node/FetchMobile/pose", PoseStamped, callback)
+    rospy.on_shutdown(save_to_csv)
+    rospy.loginfo("Listening to /vrpn_client_node/FetchMobile/pose ... Press Ctrl+C to stop.")
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
